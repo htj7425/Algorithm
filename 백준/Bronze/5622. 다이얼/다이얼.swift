18a7d742c3ee0b1f92dbd46bcfee7ca61b5422c5
TAEJUN <43903354+htj7425@users.noreply.github.com>
2023-09-29 15:59:45 +0900

    [Bronze II] Title: 다이얼, Time: 8 ms, Memory: 69096 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.swift
@@ -0,0 +1,25 @@
+var answer = 0
+for i in readLine()!{
+    switch i {
+    case "A","B","C":
+        answer += 3
+    case "D","E","F":
+        answer += 4
+    case "G","H","I":
+        answer += 5
+    case "J","K","L":
+        answer += 6
+    case "M","N","O":
+        answer += 7
+    case "P","Q","R","S":
+        answer += 8
+    case "T","U","V":
+        answer += 9
+    case "W","X","Y","Z":
+        answer += 10
+    default:
+        break
+    }
+}
+
+print(answer)

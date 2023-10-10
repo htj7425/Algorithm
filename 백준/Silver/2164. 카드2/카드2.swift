9a1b66502cd266bf6e11b0a0d091e5245c450eb6
TAEJUN <43903354+htj7425@users.noreply.github.com>
2023-10-10 09:23:43 +0900

    [Silver IV] Title: 카드2, Time: 32 ms, Memory: 91232 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.swift
@@ -0,0 +1,69 @@
+import Foundation
+
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> Int {
+        var str = 0
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += Int(now)
+            now = read()
+        }
+        
+        return str
+    }
+}
+
+let file = FileIO()
+
+let n = file.readInt()
+var index = 0
+var queue = Array(1...n)
+
+var flag = true
+while queue.count - index != 1 {
+    if flag {
+        index += 1
+        flag = false
+    } else {
+        queue.append(queue[index])
+        index += 1
+        flag = true
+    }
+}
+
+print(queue[index])

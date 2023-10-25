245ce9420f07e6c9a2210bfddaaebe66b2d97255
TAEJUN <43903354+htj7425@users.noreply.github.com>
2023-10-25 09:49:46 +0900

    [Silver II] Title: 부분수열의 합, Time: 640 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.swift
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.swift
@@ -8,18 +8,18 @@ var count = 0
 
 for i in 1...n {
     stack = [Int]()
-    recur(0, i)
+    recur(0, i, 0)
 }
 
-func recur(_ start: Int,_ endCount: Int) {
+func recur(_ start: Int,_ endCount: Int, _ sum: Int) {
     if stack.count == endCount {
-        if stack.reduce(0, +) == s { count += 1 }
+        if sum == s { count += 1 }
         return
     }
     
     for i in start..<nums.count {
         stack.append(nums[i])
-        recur(i + 1, endCount)
+        recur(i + 1, endCount, sum + nums[i])
         stack.removeLast()
     }
 }
